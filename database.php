24627b7a3ff0e3c2a97987cd6e3822d3e157d017
Ilsoda Mbungele <Ilsoda.Mbungele@enca.com>
2016-06-22 22:54:11 +0200

    Using PDO for my database connection

--- database.php
+++ database.php
@@ -1,10 +1,9 @@
 <?php
-if(!mysql_connect("localhost","root","root"))
-{
-     die('Can not connect the database, look at: '.mysql_error());
-}
-if(!mysql_select_db("smiggle"))
-{
-     die('Can not find the database, look at:  '.mysql_error());
-}
+
+$host = "localhost";
+$user = "root";
+$pass = "root";
+$database = "smiggle";
+
+$connection = new mysqli($host, $user, $pass, $database);
 ?>
